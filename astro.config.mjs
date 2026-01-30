cf339308df72caf378675e473eb9aaffb071c35b
SinagaPande <jonathan.123140153@student.itera.ac.id>
2026-01-30 14:29:40 +0700

    konfigurasi site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   // Update domain ke Vercel
   site: 'https://sukadanailir.vercel.app', 
-  integrations: [tailwind()]
+  integrations: [tailwind(), sitemap()]
 });
