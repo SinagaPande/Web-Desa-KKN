af6af193e15c3548831ec6821f165236874eb2ca
SinagaPande <jonathan.123140153@student.itera.ac.id>
2026-02-02 01:09:23 +0700

    Program & Kegiatan Desa

---
+++ src/components/ProgramKegiatan/programData.js
@@ -0,0 +1,53 @@
+export const programData = {
+    kegiatan: {
+        title: "Dokumentasi Kegiatan",
+        events: [
+            {
+                name: "Penyaluran BLT DD 2025",
+                images: [
+                    "/Imagery/Program & Kegiatan/BLT DD 2025/1_BLT.webp",
+                    "/Imagery/Program & Kegiatan/BLT DD 2025/2_BLT.webp",
+                    "/Imagery/Program & Kegiatan/BLT DD 2025/3_BLT.webp",
+                    "/Imagery/Program & Kegiatan/BLT DD 2025/4_BLT.webp",
+                    "/Imagery/Program & Kegiatan/BLT DD 2025/5_BLT.webp",
+                    "/Imagery/Program & Kegiatan/BLT DD 2025/6_BLT.webp",
+                    "/Imagery/Program & Kegiatan/BLT DD 2025/7_BLT.webp",
+                    "/Imagery/Program & Kegiatan/BLT DD 2025/8_BLT.webp"
+                ]
+            },
+            {
+                name: "Bantuan Sembako Janda Lansia & Anak Yatim 2024",
+                images: [
+                    "/Imagery/Program & Kegiatan/Bantuan sembako janda lansia dan anak yatim 2024/1_BantuanSembako.webp",
+                    "/Imagery/Program & Kegiatan/Bantuan sembako janda lansia dan anak yatim 2024/2_BantuanSembako.webp",
+                    "/Imagery/Program & Kegiatan/Bantuan sembako janda lansia dan anak yatim 2024/3_BantuanSembako.webp",
+                    "/Imagery/Program & Kegiatan/Bantuan sembako janda lansia dan anak yatim 2024/4_BantuanSembako.webp",
+                    "/Imagery/Program & Kegiatan/Bantuan sembako janda lansia dan anak yatim 2024/5_BantuanSembako.webp",
+                    "/Imagery/Program & Kegiatan/Bantuan sembako janda lansia dan anak yatim 2024/6_BantuanSembako.webp",
+                    "/Imagery/Program & Kegiatan/Bantuan sembako janda lansia dan anak yatim 2024/7_BantuanSembako.webp",
+                    "/Imagery/Program & Kegiatan/Bantuan sembako janda lansia dan anak yatim 2024/8_BantuanSembako.webp",
+                    "/Imagery/Program & Kegiatan/Bantuan sembako janda lansia dan anak yatim 2024/9_BantuanSembako.webp",
+                    "/Imagery/Program & Kegiatan/Bantuan sembako janda lansia dan anak yatim 2024/10_BantuanSembako.webp",
+                    "/Imagery/Program & Kegiatan/Bantuan sembako janda lansia dan anak yatim 2024/11_BantuanSembako.webp"
+                ]
+            },
+            {
+                name: "Bantuan Termos Ibu PKK",
+                images: [
+                    "/Imagery/Program & Kegiatan/Bantuan tremos ibu pkk/1_Bantuan Termos.webp",
+                    "/Imagery/Program & Kegiatan/Bantuan tremos ibu pkk/2_Bantuan Termos.webp",
+                    "/Imagery/Program & Kegiatan/Bantuan tremos ibu pkk/3_Bantuan Termos.webp",
+                ]
+            },
+            {
+                name: "Pembagian Pupuk Cair",
+                images: [
+                    "/Imagery/Program & Kegiatan/Pembagian pupuk cair/1_Pupuk cair.webp",
+                    "/Imagery/Program & Kegiatan/Pembagian pupuk cair/2_Pupuk cair.webp",
+                    "/Imagery/Program & Kegiatan/Pembagian pupuk cair/3_Pupuk cair.webp",
+                    "/Imagery/Program & Kegiatan/Pembagian pupuk cair/4_Pupuk cair.webp"
+                ]
+            }
+        ]
+    }
+};
