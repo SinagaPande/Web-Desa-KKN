657eb843ee9a56acac6513d20ca749f8c7dab498
SinagaPande <jonathan.123140153@student.itera.ac.id>
2026-02-01 08:50:51 +0700

    Penambahan halaman pendidikan dan menggunakan satu Lightbox Global

---
+++ src/components/Pendidikan/educationData.js
@@ -0,0 +1,84 @@
+// src/components/Pendidikan/educationData.js
+
+export const educationData = {
+    tk: {
+        title: "TK & PAUD",
+        schools: [
+            {
+                name: "TK Bunda Delima",
+                images: [
+                    "/Imagery/Pendidikan/TK-PAUD/TK Bunda Delima/1_TK.webp",
+                    "/Imagery/Pendidikan/TK-PAUD/TK Bunda Delima/2_TK.webp",
+                    "/Imagery/Pendidikan/TK-PAUD/TK Bunda Delima/3_TK.webp",
+                    "/Imagery/Pendidikan/TK-PAUD/TK Bunda Delima/4_TK.webp"
+                ],
+                locationLink: "https://maps.app.goo.gl/zKjbUqgQcbfUPEZQ7",
+                facebookLink: "" 
+            },
+            {
+                name: "Paud Mandiri",
+                images: [
+                    "/Imagery/Pendidikan/TK-PAUD/Paud Mandiri/1_PaudRK 3.webp",
+                    "/Imagery/Pendidikan/TK-PAUD/Paud Mandiri/2_PaudRK 3.webp",
+                    "/Imagery/Pendidikan/TK-PAUD/Paud Mandiri/3_PaudRK 3.webp",
+                    "/Imagery/Pendidikan/TK-PAUD/Paud Mandiri/4_PaudRK 3.webp",
+                    "/Imagery/Pendidikan/TK-PAUD/Paud Mandiri/5_PaudRK 3.webp",
+                    "/Imagery/Pendidikan/TK-PAUD/Paud Mandiri/6_PaudRK 3.webp"
+                ],
+                locationLink: "https://goo.gl/maps/Q4j6YR8Kcewrd8388",
+                facebookLink: ""
+            }
+        ]
+    },
+    sd: {
+        title: "Sekolah Dasar",
+        schools: [
+            {
+                name: "SD 01",
+                images: [
+                    "/Imagery/Pendidikan/SD/SD 01/1_SDN1.webp",
+                    "/Imagery/Pendidikan/SD/SD 01/2_SDN1.webp",
+                    "/Imagery/Pendidikan/SD/SD 01/3_SDN1.webp",
+                    "/Imagery/Pendidikan/SD/SD 01/4_SDN1.webp",
+                    "/Imagery/Pendidikan/SD/SD 01/5_SDN1.webp",
+                    "/Imagery/Pendidikan/SD/SD 01/6_SDN1.webp"
+                ],
+                locationLink: "https://maps.app.goo.gl/WQjj7PH4bvQeszKp9",
+                facebookLink: ""
+            },
+            {
+                name: "SD 02",
+                images: [
+                    "/Imagery/Pendidikan/SD/SD 02/1_SDN2.webp",
+                    "/Imagery/Pendidikan/SD/SD 02/2_SDN2.webp",
+                    "/Imagery/Pendidikan/SD/SD 02/3_SDN2.webp",
+                    "/Imagery/Pendidikan/SD/SD 02/4_SDN2.webp",
+                    "/Imagery/Pendidikan/SD/SD 02/5_SDN2.webp",
+                    "/Imagery/Pendidikan/SD/SD 02/6_SDN2.webp",
+                    "/Imagery/Pendidikan/SD/SD 02/7_SDN2.webp"
+                ],
+                locationLink: "https://maps.app.goo.gl/KnCt6KKohyKzNp3m9",
+                facebookLink: "https://www.facebook.com/share/1CBsgknEe9/"
+            }
+        ]
+    },
+    smp: {
+        title: "SMP",
+        schools: [
+            {
+                name: "SMP Perintis",
+                images: [
+                    "/Imagery/Pendidikan/SMP/1_SMPPerintis.webp",
+                    "/Imagery/Pendidikan/SMP/2_SMPPerintis.webp",
+                    "/Imagery/Pendidikan/SMP/3_SMPPerintis.webp",
+                    "/Imagery/Pendidikan/SMP/4_SMPPerintis.webp",
+                    "/Imagery/Pendidikan/SMP/5_SMPPerintis.webp",
+                    "/Imagery/Pendidikan/SMP/6_SMPPerintis.webp",
+                    "/Imagery/Pendidikan/SMP/7_SMPPerintis.webp"
+                ],
+                locationLink: "https://maps.app.goo.gl/PAbuikq3kZA815as9",
+                facebookLink: ""
+            }
+        ]
+    }
+};
