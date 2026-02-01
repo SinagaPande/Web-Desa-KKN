635d60af4900712eac7c77be3ea0b7b95cc4437c
SinagaPande <jonathan.123140153@student.itera.ac.id>
2026-02-01 23:32:36 +0700

    Penambahan halaman keagamaan

---
+++ src/components/Keagamaan/keagamaanData.js
@@ -0,0 +1,24 @@
+export const keagamaanData = {
+    ibadah: {
+        title: "Sarana Ibadah",
+        places: [
+            {
+                name: "Masjid Al Muttaqin",
+                images: [
+                    "/Imagery/RumahIbadah/Masjid Al Muttaqin/1_RK3.webp",
+                    "/Imagery/RumahIbadah/Masjid Al Muttaqin/2_RK3.webp"
+                ],
+                locationLink: "https://maps.app.goo.gl/BLB3BwaDA4fLAU938",
+            },
+            {
+                name: "Masjid At-Taqwa",
+                images: [
+                    "/Imagery/RumahIbadah/Masjid At Taqwa/1_RK2.webp",
+                    "/Imagery/RumahIbadah/Masjid At Taqwa/2_RK2.webp",
+                    "/Imagery/RumahIbadah/Masjid At Taqwa/3_RK2.webp"
+                ],
+                locationLink: "https://maps.app.goo.gl/WVKpzwcmHNDwfjaTA",
+            }
+        ]
+    }
+};
